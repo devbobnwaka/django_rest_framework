dd38d9787d92b5f659ad1b8b4b311041c5622708
devbobnwaka <nwakachibueze@gmail.com>
2023-02-18 17:34:46 +0100

    Token Authentication

--- py_client/create.py
+++ py_client/create.py
@@ -3,6 +3,7 @@ import requests
 
 endpoint:str = "http://localhost:8000/api/products/"
 
+headers = {'Authorization': 'Bearer 2f1e299ef94dd7f2bf4771c3a8f50cecf9569448'}
 data = {
     "title": "This field is done!!!"
 }
--- py_client/list.py
+++ py_client/list.py
@@ -1,9 +1,21 @@
 import requests
+from getpass import getpass
 
+auth_endpoint:str = "http://localhost:8000/api/auth/"
+username = input("Please enter your username: ")
+password = getpass('Please enter your password: ')
+auth_response = requests.post(auth_endpoint, json={"username":username, "password": password }) 
 
-endpoint:str = "http://localhost:8000/api/products/"
+print(auth_response.json())
 
-get_response = requests.get(endpoint) 
+if auth_response.status_code == 200:
+    token = auth_response.json()['token']
+    headers = {
+        "Authorization":f"Bearer {token}"
+    }
+    endpoint:str = "http://localhost:8000/api/products/"
 
-print(get_response.json())
+    get_response = requests.get(endpoint, headers=headers ) 
+
+    print(get_response.json())
 
