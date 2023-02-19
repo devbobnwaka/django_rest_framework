3e86887ecfa73fcf41651b707a2203f7f7fab544
devbobnwaka <nwakachibueze@gmail.com>
2023-02-19 16:03:31 +0100

    Custom Validation with Serializers

--- backend/products/serializers.py
+++ backend/products/serializers.py
@@ -1,6 +1,9 @@
 from rest_framework import serializers
 from rest_framework.reverse import reverse
 from .models import Product
+# from .validators import (validate_title, validate_title_no_hello, unique_product_title)
+from . import validators
+
 
 
 class ProductSerializer(serializers.ModelSerializer):
@@ -8,12 +11,18 @@ class ProductSerializer(serializers.ModelSerializer):
     edit_url = serializers.SerializerMethodField(read_only = True)
     url = serializers.HyperlinkedIdentityField(view_name='product-detail', lookup_field='pk')
     # email = serializers.EmailField(write_only=True)
+    title = serializers.CharField(validators=[
+        validators.validate_title_no_hello, 
+        validators.unique_product_title
+        ]) 
+    # name = serializers.CharField(source='title', read_only=True)
     class Meta:
         model = Product
         fields = [
             'pk',
             'edit_url',
             'url', 
+            # 'name',
             # 'email', 
             'title', 
             'content', 
@@ -21,6 +30,14 @@ class ProductSerializer(serializers.ModelSerializer):
             'sale_price', 
             'discount',
             ]
+
+    # def validate_title(self, value):
+    #     request = self.context.get('request')
+    #     user = request.user
+    #     qs = Product.objects.filter(user=user, title__iexact=value)
+    #     if qs.exists():
+    #         raise serializers.ValidationError(f"{value} is already a product name.")
+    #     return value
 
     # def get_url(self, obj):
     #     # return f"/api/products/{obj.pk}/"
