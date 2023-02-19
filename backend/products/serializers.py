0a182d549cb8722a53b9fedb9376e497e7332d5e
devbobnwaka <nwakachibueze@gmail.com>
2023-02-19 20:19:45 +0100

    Pagination

--- backend/products/serializers.py
+++ backend/products/serializers.py
@@ -1,13 +1,21 @@
 from rest_framework import serializers
+from api.serializers import UserPublicSerializer
 from rest_framework.reverse import reverse
 from .models import Product
 # from .validators import (validate_title, validate_title_no_hello, unique_product_title)
 from . import validators
 
 
-
+class ProductInlineSerializer(serializers.Serializer):
+    url = serializers.HyperlinkedIdentityField(view_name='product-detail', lookup_field='pk', read_only=True)
+    title = serializers.CharField(read_only=True)
+ 
 class ProductSerializer(serializers.ModelSerializer):
-    discount = serializers.SerializerMethodField(read_only=True)
+    # user = UserPublicSerializer(read_only=True)
+    owner = UserPublicSerializer(source='user', read_only=True)
+    # related_products = ProductInlineSerializer(source='user.product_set.all', read_only=True, many=True)
+    # my_user_data = serializers.SerializerMethodField(read_only=True)
+    # discount = serializers.SerializerMethodField(read_only=True)
     edit_url = serializers.SerializerMethodField(read_only = True)
     url = serializers.HyperlinkedIdentityField(view_name='product-detail', lookup_field='pk')
     # email = serializers.EmailField(write_only=True)
@@ -20,18 +28,24 @@ class ProductSerializer(serializers.ModelSerializer):
         model = Product
         fields = [
             # 'user',
+            'owner',
             'pk',
             'edit_url',
-            'url', 
+            'url',
             # 'name',
             # 'email', 
             'title', 
             'content', 
             'price', 
             'sale_price', 
-            'discount',
+            # 'discount',
+            # 'my_user_data',
+            # 'related_products',
             ]
-
+    def get_my_user_data(self, obj):
+        return {
+            "username": obj.user.username,
+        }
     # def validate_title(self, value):
     #     request = self.context.get('request')
     #     user = request.user
@@ -54,12 +68,12 @@ class ProductSerializer(serializers.ModelSerializer):
         return reverse("product-edit", kwargs={"pk":obj.pk}, request=request)
 
 
-    def get_discount(self, obj):
-        if not hasattr(obj, 'id'):
-            return None
-        if not isinstance(obj, Product):
-            return None
-        return obj.get_discount()
+    # def get_discount(self, obj):
+    #     if not hasattr(obj, 'id'):
+    #         return None
+    #     if not isinstance(obj, Product):
+    #         return None
+    #     return obj.get_discount()
        
 
     # def create(self, validated_data):
